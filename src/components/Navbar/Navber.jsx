94279e58b08df83df3b813afa03e2c41261d99d5
aman27052002 <kothariaman2705@gmail.com>
2024-07-16 22:20:09 +0530

    food ordering app

---
+++ src/components/Navbar/Navber.jsx
@@ -0,0 +1,33 @@
+import React,{useState,useContext} from "react";
+import "./Navbar.css";
+import { assets } from "../../assets/assets.js";
+import { Link } from "react-router-dom";
+import { StoreContext } from "../../context/StoreContext.jsx";
+
+const Navber = ({setShowLogin}) => {
+  const [menu,setMenu] = useState("Home");
+  const {getTotalCartAmount} = useContext(StoreContext)
+
+
+  return (
+    <div className="navbar">
+      <Link to='/'><img className="logo" src={assets.logo} alt="" /></Link>
+      <ul className="navbar-menu">
+        <Link to='/' onClick={()=>setMenu("Home")} className={menu==="Home"?"active":""}>Home</Link>
+        <a href="#explore-menu" onClick={()=>setMenu("Menu")} className={menu==="Menu"?"active":""}>Menu</a>
+        <a href="#app-download" onClick={()=>setMenu("Mobile-app")} className={menu==="Mobile-app"?"active":""}>Mobile-app</a>
+        <a href="#footer" onClick={()=>setMenu("Contact us")} className={menu==="Contact us"?"active":""}>Contact us</a>
+      </ul>
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt="" />
+        <div className="navbar-search-icon">
+          <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link>
+          <div className={getTotalCartAmount() === 0 ?"":"dot"}></div>
+        </div>
+        <button onClick={()=>setShowLogin(true)}>Sign in</button>
+      </div>
+    </div>
+  );
+};
+
+export default Navber;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,19 @@
+import React,{useState} from 'react'
+import "./Home.css"
+import Header from "../../components/Header/Header.jsx"
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu.jsx'
+import FoodDisplay from '../../components/FoodDisplay/FoodDisplay.jsx'
+import AppDownload from '../../components/AppDownload/AppDowload.jsx'
+const Home = () => {
+  const [category,setCategory] = useState("All")
+  return (
+    <div>
+      <Header/>
+      <ExploreMenu category={category} setCategory={setCategory}/>
+      <FoodDisplay category={category}/>
+      <AppDownload/>
+    </div>
+  )
+}
+
+export default Home
